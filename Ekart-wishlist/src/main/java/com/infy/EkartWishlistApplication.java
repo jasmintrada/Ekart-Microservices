43b073feb269dc0dd8897c0bf4b5e97cce2edaad
jasmintrada <jasmintrada168@gmail.com>
2020-05-03 16:21:48 +0530

    Wishlist microservice

---
+++ Ekart-wishlist/src/main/java/com/infy/EkartWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartWishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartWishlistApplication.class, args);
+	}
+
+}
